a3777637d91e9689f8be68dc23b2a63a74030995
sylviayuan-sy <luyuan@stanford.edu>
2021-05-14 08:23:00 -0700

    updated baseline model, moved helper functions to util.py

--- pianoGAN.py
+++ pianoGAN.py
@@ -3,13 +3,11 @@ from tensorflow.keras.layers import Activation, UpSampling2D, Reshape, Input, Co
 from tensorflow.keras.models import Model
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
-import librosa.display
 import numpy as np
-import os
-import cv2
 import pandas as pd
 import scipy.io.wavfile
 import time
+import util
 
 
 MODEL_DIR = "./saved_models/"
@@ -20,38 +18,45 @@ sampling_rate = 22500
 eps = 1e-7
 beta = 0.9
 
-df = pd.read_csv("./data/saved_mean_std.csv")
-means = df['mean']
-stds = df['std']
-mag_mean = 0
-for index in range(means.shape[0]):
-    if means[index] != -np.inf:
-        mag_mean += means[index] / means.shape[0]
-mag_std = np.mean(stds)
 losses = {"epoch": [], "d_loss": [], "d_acc": [], "g_loss": []}
 
 
-# gan overall structure from CS230 lecture slides by Kian Katanforoosh
+'''
+Based on and modified from: 
+    Title: Keras-GAN
+    Author: Erik Linder-Norén
+    Date: 2017
+    Code version: -
+    Availability: https://github.com/eriklindernoren/Keras-GAN.git
+'''
 class pianoGAN():
     def __init__(self):
+
+        # define input size
         self.height = 256
         self.width = 256
         self.latent_dim = 100
         self.shape = (self.height, self.width, 1)
 
-        # https://machinelearningmastery.com/how-to-train-stable-generative-adversarial-networks/
+        # define optimizer
         optimizer = Adam(learning_rate=0.0001, beta_1=0.5, beta_2=0.9, clipnorm=1)
 
+        # build discriminator
         self.discriminator = self.build_discriminator()
         self.discriminator.compile(loss='binary_crossentropy',
                                    optimizer=optimizer,
                                    metrics=['accuracy'])
-
+        # build generator
         self.generator = self.build_generator()
 
+        # construct noisy data
         z = Input(shape=(self.latent_dim,))
         matrix = self.generator(z)
+
+        # we do not train the discriminator
         self.discriminator.trainable = False
+
+        # test validity of input
         validity = self.discriminator(matrix)
         self.combined = Model(z, validity)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
@@ -121,7 +126,9 @@ class pianoGAN():
 
     def train(self, epochs, batch_size=128, sample_interval=50):
         t = time.time()
-        paths = self.get_dataset_paths(STFT_DIR, ".npy")
+
+        # acquire training data
+        paths = util.get_dataset_paths(STFT_DIR, ".npy")
         all_arrays = []
         for path in paths:
             all_arrays.append(np.load(path))
@@ -129,38 +136,51 @@ class pianoGAN():
 
         print("All training data loaded, took", time.time() - t, "seconds.")
 
+        # create valid and fake true labels
         valid = np.ones((batch_size, 1))
         fake = np.zeros((batch_size, 1))
 
+        # run each epoch
         for epoch in range(epochs):
             t = time.time()
+
+            # randomly select one batch of training data points
             idx = np.random.randint(0, S_train.shape[0], batch_size)
             matrices = S_train[idx]
             noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
 
+            # run generator and discriminator, record loss and accuracy
             gen_matrices = self.generator.predict(noise)
             d_loss_real = self.discriminator.train_on_batch(matrices, valid)
             d_loss_fake = self.discriminator.train_on_batch(gen_matrices, fake)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-
             noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
             g_loss = self.combined.train_on_batch(noise, valid)
 
             print("Finished epoch", epoch, "after", time.time() - t, "seconds")
             print("Discriminator loss and accuracy:", d_loss, "Generator loss:", g_loss)
 
+            # if current epoch is a saving epoch
             if epoch % sample_interval == 0:
+
+                # save the generator and discriminator
                 self.generator.save(MODEL_DIR + "generator")
                 self.discriminator.save(MODEL_DIR + "discriminator")
+
+                # revert generated stft to wav file and save to output folder
                 out = AUDIO_OUT_DIR + "epoch_" + str(epoch) + ".wav"
                 try:
-                    y = self.audio_reconstruction(gen_matrices[0])
+                    y = util.audio_reconstruction(gen_matrices[0])
                     scipy.io.wavfile.write(out, sampling_rate, y)
                 except:
                     print("Error in griffinlim at epoch", epoch)
                     pass
+
+                # save generated matrix to output folder
                 out = STFT_OUT_DIR + "epoch_" + str(epoch) + ".npy"
                 np.save(out, gen_matrices[0])
+
+                # record loss and accuracy
                 losses["d_loss"].append(d_loss[0])
                 losses["d_acc"].append(d_loss[1])
                 losses["g_loss"].append(g_loss)
@@ -168,31 +188,10 @@ class pianoGAN():
                 df = pd.DataFrame(losses, columns=['epoch', 'd_loss', 'd_acc', 'g_loss'])
                 df.to_pickle("./output/loss.pkl")
 
-    def get_dataset_paths(self, directory, extension):
-        paths = []
-        for subdir, dirs, files in os.walk(directory):
-            for file in files:
-                if file.lower().endswith(extension):
-                    path = os.path.join(subdir, file)
-                    paths.append(path)
-        return paths
-
-    def upsample(self, S_downsample):
-        return cv2.resize(S_downsample, (862, 1025), interpolation=cv2.INTER_LINEAR)
-
-    def audio_reconstruction(self, S):
-        S = self.upsample(np.squeeze(S))
-        S = S * 3
-        S = S * (mag_std + eps) + mag_mean
-        S = np.exp(S)
-
-        return librosa.griffinlim(S)
-
 
 if __name__ == '__main__':
     gan = pianoGAN()
-    gan.train(epochs=15000, batch_size=128, sample_interval=100)
+    gan.train(epochs=10000, batch_size=128, sample_interval=100)
     gan.generator.save(MODEL_DIR + "generator")
     gan.discriminator.save(MODEL_DIR + "discriminator")
-    df = pd.read_pickle("./output/loss.pkl")
-    print(df)
+    util.plot_loss(("./output/loss.pkl"))
---
+++ util.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import os
+import librosa.display
+
+eps = 1e-7
+
+df = pd.read_csv("./data/saved_mean_std.csv")
+means = df['mean']
+stds = df['std']
+mag_mean = 0
+for index in range(means.shape[0]):
+    if means[index] != -np.inf:
+        mag_mean += means[index] / means.shape[0]
+mag_std = np.mean(stds)
+
+def plot_loss(path):
+    df = pd.read_pickle(path)
+    epoch = df['epoch']
+    d_loss = df['d_loss']
+    d_acc = df['d_acc']
+    g_loss = df['g_loss']
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel('num_epochs')
+    ax.set_ylabel('loss')
+    ax.plot(epoch, d_loss, c='b', label='d_loss')
+    ax.plot(epoch, d_acc, c='g', label='d_acc')
+    ax.plot(epoch, g_loss, c='r', label='g_loss')
+    ax.legend()
+    plt.savefig("./loss_plot.png")
+
+
+def get_dataset_paths(directory, extension):
+    paths = []
+    for subdir, dirs, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(extension):
+                path = os.path.join(subdir, file)
+                paths.append(path)
+    return paths
+
+
+def upsample(S_downsample):
+    return cv2.resize(S_downsample, (862, 1025), interpolation=cv2.INTER_LINEAR)
+
+
+def audio_reconstruction(S):
+    S = upsample(np.squeeze(S))
+    S = S * 3
+    S = S * (mag_std + eps) + mag_mean
+    S = np.exp(S)
+
+    return librosa.griffinlim(S)
